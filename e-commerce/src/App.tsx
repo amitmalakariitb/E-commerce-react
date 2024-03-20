ed532c7ca4ea0ce46d59fa0a45fd68306e69cb10
Amit <amitmalakar887@gmail.com>
2024-03-21 02:57:46 +0530

    Solved merge conflicts

--- e-commerce/src/App.tsx
+++ e-commerce/src/App.tsx
@@ -1,8 +1,6 @@
-import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
-import Footer from './components/footer/Footer' 
 
 
 // just for checking 
@@ -12,37 +10,15 @@ import Checkout from './components/checkout/checkout';
 import Confirmation from './components/confirmation/confirmation';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-
-      {/* <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <div>
-        {/* <img src="C:\Users\udayd\OneDrive\Desktop\frontend\E-commerce-react\e-commerce\src\tree.jpg" alt="" /> */}
-        {/* hello how are you 
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum, reiciendis eos quia, optio vero doloribus officiis consectetur asperiores nostrum quas ipsa nemo cupiditate nulla corporis minima corrupti cumque libero dicta?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui nam consectetur asperiores quas at minima totam alias magni sit voluptatibus, cum ratione dolorem adipisci distinctio nulla nisi saepe provident perferendis animi incidunt nemo tempore veniam sapiente. Autem dolorem laboriosam, quisquam aliquid temporibus alias ut doloremque eum, aperiam beatae, quae accusamus reiciendis. Reiciendis unde quis nostrum dolorum doloremque assumenda delectus amet neque, ducimus excepturi voluptatibus consectetur? Nihil qui laborum dolorum. Dolorum nemo cum quia eaque cumque recusandae voluptas dolores maxime facilis commodi hic molestiae iste, sint vel. Maxime, eligendi. Eos cumque aperiam suscipit voluptate laudantium incidunt dolor autem facilis id! Natus, exercitationem.
-        lorem10001
-      </div>
-      <Footer></Footer> */} 
-
-      {/* <ProductDetails></ProductDetails> */}
-
       <Router>
-      <Routes>
-        {/* Route for the ProductDetails component */}
-        <Route path="/product-details" Component={ProductDetails} />
-
-        {/* Route for the Checkout component */}
-        <Route path="/checkout" Component={Checkout} />
-        {/* Route for the order confirmation component */}
-        <Route path='/confirmation' Component={Confirmation}></Route>
-      </Routes>
-      {/* <Footer /> */}
-    </Router>
+        <Routes>
+          <Route path="/product-details" Component={ProductDetails} />
+          <Route path="/checkout" Component={Checkout} />
+          <Route path='/confirmation' Component={Confirmation}></Route>
+        </Routes>
+      </Router>
 
     </>
   )
